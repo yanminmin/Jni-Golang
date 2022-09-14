48683fe523e2d22e023b535e21896a1851fd581e
Martyn Yan <minmin.yan@okg.com>
2022-09-14 11:33:26 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 rm Hello.class libHello.h libHello.so
-go build -buildmode=c-shared -o libHello.so main.go
-#javac Hello.java
-#java -Djava.library.path=. Hello
+#linux可能需要插件mod.
+#go mod init a
+#go mod tidy
+go build -buildmode=c-shared -o libHello.so
+javac Hello.java
+java -Djava.library.path=. Hello
